aee45a331f2c032a1f892186920f1e868bb313c7
Savir7253 <7253savirkhan@gmail.com>
2026-09-25 15:47:57 +0530

    Add LeetCode Easy - Divisible and Non-divisible Sums Difference

---
+++ leetcode/Easy/3172-Divisible-And-Non-Divisible-Sums-Difference/solution.java
@@ -0,0 +1,21 @@
+// LeetCode Problem: Divisible and Non-divisible Sums Difference
+// Link: https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int num1=0;
+        int num2=0;
+        for(int i=1;i<=n;i++){
+            if(i%m==0){
+                num2 +=i;
+            }
+            else{
+                num1+=i;
+            }
+        }
+        return num1-num2;
+        
+    }
+}
